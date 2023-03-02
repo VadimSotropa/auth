8ffb47bc3e9aab46269598ddb3b1622c7fffc11f
Vadim <vadimsotropa@gmail.coml>
2023-03-02 15:43:35 +0100

    adding the possibility ro push to array the crypro id

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const User = require('./models/user');
-const Article = require('./models/article');
-
+const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+const bcrypt = require("bcrypt");
+const uid2 = require('uid2');
+const jwt = require("jsonwebtoken");
+const auth = require("./auth");
+// require database connection 
+const dbConnect = require("./db/dbConnect");
 
-app.use(express.json());
+// execute database connection 
+dbConnect();
+const User = require("./db/userModel");
+const Liked = require("./db/userLiked")
 
-mongoose.connect('mongodb://localhost/myapp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+// Google cod part
+
+
+
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+// body parser configuration
+app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (request, response, next) => {
+  response.json({ message: "Hey! This is your server response!" });
+  next();
 });
 
 app.post("/register", (request, response) => {
@@ -25,8 +52,10 @@ app.post("/register", (request, response) => {
         name: request.body.name,
         token: uid2(32),
         canAddFavorite: true,
-        likedArticles: [], // Initialize likedArticles array to empty
+       
       });
+
+      // save the new user
       user
         .save()
         // return success if the new user is added to the database successfully
@@ -43,72 +72,60 @@ app.post("/register", (request, response) => {
             error,
           });
         });
-      })
-      // catch error if the password hash isn't successful
-      .catch((e) => {
-        response.status(500).send({
-          message: "Password was not hashed successfully",
-          e,
-        });
+    })
+    // catch error if the password hash isn't successful
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
       });
     });
+});
 
+// login endpoint
+app.post("/login", (request, response) => {
+  // check if email exists
+  User.findOne({ email: request.body.email })
 
+    // if email exists
+    .then((user) => {
+      // compare the password entered and the hashed password found
+      bcrypt
+        .compare(request.body.password, user.password)
 
+        // if the passwords match
+        .then((passwordCheck) => {
 
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
-
-  User.findOne({ email: email })
-    .then(user => {
-      if (!user) {
-        res.status(401).send({
-          message: 'Authentication failed',
-        });
-      } else {
-        bcrypt.compare(password, user.password, (err, result) => {
-          if (err || !result) {
-            res.status(401).send({
-              message: 'Authentication failed',
-            });
-          } else {
-            res.status(200).send({
-              message: 'Login successful',
-              id: user._id,
+          // check if password matches
+          if(!passwordCheck) {
+            return response.status(400).send({
+              message: "Passwords does not match",
+              error,
             });
           }
+          response.status(200).send({
+            message: "Login Successful",
+            email: user.email,
+            token,
+          });
+        })
+        // catch error if password does not match
+        .catch((error) => {
+          response.status(400).send({
+            message: "Passwords does not match",
+            error,
+          });
         });
-      }
     })
-    .catch(err => {
-      res.status(500).send(err);
+    // catch error if email does not exist
+    .catch((e) => {
+      response.status(404).send({
+        message: "Email not found",
+        e,
+      });
     });
 });
 
-app.post('/users/:token/favorites', (req, res) => {
-  const { token } = req.params;
-  const { cryptoId } = req.body;
-
-  User.findById(token, (err, user) => {
-    if (err || !user) {
-      res.status(404).send({
-        message: 'User not found',
-      });
-    } else {
-      user.likedArticles.push(cryptoId);
-      user.save((err, result) => {
-        if (err) {
-          res.status(500).send(err);
-        } else {
-          res.status(201).send({
-            message: 'Favorite added successfully',
-            likedArticle: cryptoId,
-          });
-        }
-      });
-    }
-  });
-});
 // free endpoint
 app.get("/free-endpoint", (request, response) => {
   response.json({ message: "You are free to access me anytime" });
