8773992f7fede22cf9c10e92d17f74de10595c51
Vadim <vadimsotropa@gmail.coml>
2023-03-04 17:32:55 +0100

    route to delete from favorite

--- db/userLiked.js
+++ db/userLiked.js
@@ -7,6 +7,11 @@ const ArticleSchema = new mongoose.Schema({
     unique: true,
   },
 
+  User: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Users'
+  }],
+
 });
 
 module.exports = mongoose.model('Article', ArticleSchema);
